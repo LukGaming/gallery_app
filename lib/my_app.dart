7057ce1294a0c942ad2814de1d3da86363ad995b
Paulo <thelukgaming333@gmail.com>
2025-07-08 13:52:10 -0300

    Iniciando formulário

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:gallery_app/views/algums/albums_page.dart';
-import 'package:gallery_app/views/home_page/top_menu.dart';
-import 'package:gallery_app/views/photos/photos_grid_view.dart';
+import 'package:gallery_app/views/home_page/home_page.dart';
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -11,7 +9,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,42 +16,7 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Scaffold(
-        bottomNavigationBar: BottomNavigationBar(
-          selectedIconTheme: IconThemeData(
-            size: 40,
-            color: Colors.greenAccent,
-          ),
-          elevation: 16,
-          currentIndex: currentIndex,
-          onTap: (value) {
-            currentIndex = value;
-            setState(() {});
-          },
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.album),
-              label: "Albuns",
-            ),
-            BottomNavigationBarItem(
-              label: "Fotos",
-              icon: Icon(Icons.photo),
-            ),
-          ],
-        ),
-        body: SafeArea(
-          child: Column(
-            children: [
-              TopMenu(),
-              currentIndex == 0
-                  ? Expanded(
-                      child: AlbumList(),
-                    )
-                  : PhotosGridView(),
-            ],
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
---
+++ lib/views/album_form/album_form.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AlbumForm extends StatefulWidget {
+  const AlbumForm({super.key});
+
+  @override
+  State<AlbumForm> createState() => _AlbumFormState();
+}
+
+class _AlbumFormState extends State<AlbumForm> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
