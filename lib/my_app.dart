f340cbdf5d1b867002f4e7e16f3ea5dd9edd53ae
Paulo <thelukgaming333@gmail.com>
2025-07-08 13:00:54 -0300

    Iniciando branch

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gallery_app/views/home_page/photo_album_widget.dart';
+import 'package:gallery_app/views/home_page/top_menu.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,7 +12,26 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const Scaffold(),
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              TopMenu(),
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: 2,
+                  ),
+
+                  itemBuilder: (context, index) {
+                    return PhotoAlbumCard();
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
